19459792e4bc87f8efe6e0305751ac49ce1eccd4
Duy Truong <nmdtruong18032004@gmail.com>
2023-12-21 02:10:15 +0700

    update

--- Main.py
+++ Main.py
@@ -145,6 +145,7 @@ mean = 0
 stdev = 0
 for i in salary:
     mean += i
+print(mean)
 mean /= len(salary)
 for i in salary:
     stdev += (i - mean) ** 2
@@ -152,12 +153,12 @@ stdev = stdev ** (1 / 2) / len(salary)
 print(mean, stdev)
 
 # cau 5
-exam_30_stt = [random.randint(0, len(gender) - 1) for i in range(30)]
-exam_30_gender=[gender[i] for i in exam_30_stt]
-exam_30_salary=[salary[i] for i in exam_30_stt]
-exam_15_stt = [random.randint(0, len(gender) - 1) for i in range(15)]
-exam_15_gender=[gender[i] for i in exam_15_stt]
-exam_15_salary=[salary[i] for i in exam_15_stt]
-print(exam_15_stt)
-print(exam_15_gender)
-print(exam_15_salary)
+# exam_30_stt = [random.randint(0, len(gender) - 1) for i in range(30)]
+# exam_30_gender=[gender[i] for i in exam_30_stt]
+# exam_30_salary=[salary[i] for i in exam_30_stt]
+# exam_15_stt = [random.randint(0, len(gender) - 1) for i in range(15)]
+# exam_15_gender=[gender[i] for i in exam_15_stt]
+# exam_15_salary=[salary[i] for i in exam_15_stt]
+# print(exam_15_stt)
+# print(exam_15_gender)
+# print(exam_15_salary)
