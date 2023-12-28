f6125255f726678eaf8a1e67ea77a150c7350d4a
Duy Truong <nmdtruong18032004@gmail.com>
2023-12-29 01:57:30 +0700

    done

--- Main.py
+++ Main.py
@@ -11,18 +11,12 @@ salary = [27,61,52,69,88,85,79,99,77,165,41,83,144,74,143,131,34,
           59,46,105,61,118,114,138,24,67,130,56,99,125,87,30,119,
           40,25,44,123,45,25,94,86,128,69,102,91,106,119,139,67,
           47,62,92,124,31,49,68,109,138,105,84,86,66,128,146,59]
-# def str(a):
-#     arr=a.split(' ')
-#     s = ''
-#     for i in range(len(arr)):
-#         s += ',' + arr[i]
-#     print(s)
 
 # cau 1
 mean= st.mean(salary)
 stdev= st.pstdev(salary)
 print(mean, stdev)
-print(st.stdev(salary))
+# print(st.stdev(salary))
 # cau 2
 # female=[salary[i] for i in range(len(salary)) if gender[i] == 'W']
 # female=sorted(female)
@@ -49,7 +43,7 @@ print(st.stdev(salary))
 # exam_15=math.comb(65,15)
 # print(exam_15)
 # cau 6
-print("Mẫu 30:")
+# print("Mẫu 30:")
 # exam_30_stt = [random.randint(0, len(gender) - 1) for i in range(30)]
 # exam_30_gender=[gender[i] for i in exam_30_stt]
 # exam_30_salary=[salary[i] for i in exam_30_stt]
@@ -59,7 +53,7 @@ exam_30_salary=[91, 25, 144, 67, 40, 67, 41, 44, 138, 92, 138, 114, 130, 44, 66,
 # print(exam_30_stt)
 # print(exam_30_gender)
 # print(exam_30_salary)
-print("Mẫu 15:")
+# print("Mẫu 15:")
 # exam_15_stt = [random.randint(0, len(gender) - 1) for i in range(15)]
 # exam_15_gender=[gender[i] for i in exam_15_stt]
 # exam_15_salary=[salary[i] for i in exam_15_stt]
@@ -74,5 +68,5 @@ mean_exam_30=st.mean(exam_30_salary)
 stdev_exam_30=st.stdev(exam_30_salary)
 mean_exam_15=st.mean(exam_15_salary)
 stdev_exam_15=st.stdev(exam_15_salary)
-print(mean_exam_30,stdev_exam_30)
-print(mean_exam_15,stdev_exam_15)
+# print(mean_exam_30,stdev_exam_30)
+# print(mean_exam_15,stdev_exam_15)
